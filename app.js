75c8ab10654fc22659ad60af6b6da4268580b70e
Vadim <triv@me.com>
2019-12-03 22:36:32 -0500

    Installed nodemone + morgan

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+//Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error;
+})
 
 module.exports = app;
