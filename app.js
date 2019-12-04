4dd6f24614746f1c6ecb4b688894cdaf5d6e850a
Vadim <triv@me.com>
2019-12-03 23:48:24 -0500

    Added errors messages

--- app.js
+++ app.js
@@ -12,7 +12,18 @@ app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
 app.use((req, res, next) => {
-    const error = new Error;
-})
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 module.exports = app;
