3aaf2beaa6e760ae6706abdd03a1f249ecd2bc94
Vadim Volkov <triv@me.com>
2019-12-12 16:39:27 -0500

    add CORS method

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//CORS - Cross-Origin Resource Sharing
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH,DELETE, GET');
+        return res.status(200).json({})
+    }
+});
+
 //Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
